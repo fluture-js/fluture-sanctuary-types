b1c78382aec192a04a930f9e9dca56383944ca93
Aldwin Vlasblom <aldwin.vlasblom@gmail.com>
2017-05-13 16:13:30 +0200

    Everything

---
+++ test/index.test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var expect = require('chai').expect;
+var $ = require('sanctuary-def');
+var type = require('sanctuary-type-identifiers');
+var Future = require('fluture');
+var types = require('..');
+
+describe('Fluture Sanctuary Types', function(){
+
+  describe('FutureType', function(){
+
+    it('is a binary function', function(){
+      expect(types.FutureType).to.be.a('function');
+      expect(types.FutureType.length).to.equal(2);
+    });
+
+    it('returns a Type when given two Types', function(){
+      var actual = types.FutureType($.Unknown, $.Unknown);
+      expect(type(actual)).to.equal('sanctuary-def/Type');
+    });
+
+    it('tests positive on members', function(){
+      var Type = types.FutureType($.Unknown, $.Unknown);
+      expect($.test([], Type, Future.Par(Future.of(1)))).to.equal(false);
+      expect($.test([], Type, Future.of(1))).to.equal(true);
+    });
+
+  });
+
+  describe('ConcurrentFutureType', function(){
+
+    it('is a binary function', function(){
+      expect(types.ConcurrentFutureType).to.be.a('function');
+      expect(types.ConcurrentFutureType.length).to.equal(2);
+    });
+
+    it('returns a Type when given two Types', function(){
+      var actual = types.ConcurrentFutureType($.Unknown, $.Unknown);
+      expect(type(actual)).to.equal('sanctuary-def/Type');
+    });
+
+    it('tests positive on members', function(){
+      var Type = types.ConcurrentFutureType($.Unknown, $.Unknown);
+      expect($.test([], Type, Future.Par(Future.of(1)))).to.equal(true);
+      expect($.test([], Type, Future.of(1))).to.equal(false);
+    });
+
+  });
+
+});
+
